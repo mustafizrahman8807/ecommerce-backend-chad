8539395348ba265e2c91d53952614fb986c6d7fa
mustafizrahman8807 <mustafizrahman8807@gmail.com>
2023-10-12 19:04:51 +0600

    12 Oct 23

--- src/main/java/com/mustafij/ecommercebackend/config/MyDataRestConfig.java
+++ src/main/java/com/mustafij/ecommercebackend/config/MyDataRestConfig.java
@@ -1,7 +1,9 @@
 package com.mustafij.ecommercebackend.config;
 
+import com.mustafij.ecommercebackend.entity.Country;
 import com.mustafij.ecommercebackend.entity.Product;
 import com.mustafij.ecommercebackend.entity.ProductCategory;
+import com.mustafij.ecommercebackend.entity.State;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -20,10 +22,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     private EntityManager entityManager;
 
-   @Autowired
+    @Autowired
     public MyDataRestConfig(EntityManager theEntityManager) {
 
-       entityManager = theEntityManager;
+        entityManager = theEntityManager;
 
     }
 
@@ -38,16 +40,23 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withAssociationExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
-        //disable HTTP methods for ProductCategory: PUT, POST and DELETE
+        //disable HTTP methods for Product, ProductCategory, Country, State: PUT, POST and DELETE
 
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withAssociationExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        disabledHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+        disabledHttpMethods(Product.class, config, theUnsupportedActions);
+        disabledHttpMethods(Country.class, config, theUnsupportedActions);
+        disabledHttpMethods(State.class, config, theUnsupportedActions);
 
         // call an internal helper method
         exposeIds(config);
 
+    }
+
+    private void disabledHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withAssociationExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
 
     private void exposeIds(RepositoryRestConfiguration config) {
@@ -64,12 +73,12 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         // get the entity types for the entities
 
-        for (EntityType tempEntityType: entities) {
+        for (EntityType tempEntityType : entities) {
             entityClasses.add(tempEntityType.getJavaType());
         }
 
         // expose the entity ids for the array of entity/domain types
-        Class[] domainTypes = entityClasses.toArray(new  Class[0]);
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
         config.exposeIdsFor(domainTypes);
 
 
